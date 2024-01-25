ff20d0ae7c4898e2480bd2813d322c82d5d0e90b
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-26 01:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+function click() {
+    
+
+    const output = "First Name: " + form.fn.value + "\n" +
+                    "Last Name: " + form.ln.value + "\n" +
+                    "Phone Number: "+ form.pn.value+ "\n"+
+                    "Email ID: " + form.ei.value;
+    alert(output);
+}
+
+form.btn.addEventListener("click", click);
